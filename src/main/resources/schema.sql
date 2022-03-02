1126227968ee6f4786cd11067fd2e34473af9fe3
akash-gautam <akash0715@gmail.com>
2022-03-02 23:07:13 +0530

    created schema.sql. closes #3

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+create table person
+(
+  id integer not null,
+  name varchar(255) not null,
+  location varchar(255) not null,
+  birth_date timestamp,
+  primary key(id)
+);
+
+insert into person ( id, name, location, birth_date )
+values( 1001, 'Akash', 'Hyderabad', sysdate());
+insert into person ( id, name, location, birth_date )
+values( 1002, 'Gautam', 'Lucknow', sysdate());
+insert into person ( id, name, location, birth_date )
+values( 1003, 'Gauti', 'Noida', sysdate());
